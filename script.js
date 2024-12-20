3c6bd3fa550c10bf82171d4127c6b7a1f3deae00
Viktoria Stolz <88290448+viktoriastolz@users.noreply.github.com>
2024-12-20 21:42:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 "use strict";
 
+function removeElement(array, item) {
+  let index;
+  while ((index = array.indexOf(item)) !== -1) {
+    array.splice(index, 1); 
+  }
+}
+
+const array = [1, 3, 4, 4, 6, 2, 5, 4, 7];
+removeElement(array, 4);
+
+console.log(array); // Результат: [1, 3, 6, 2, 5, 7]
 
 
 
